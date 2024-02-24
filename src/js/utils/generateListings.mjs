175c0e291c9bf4cfb6ff01065b22145c414b4d68
Zaracki <119800248+Zaracki@users.noreply.github.com>
2024-02-24 13:22:40 +0100

    all listings are viewed properly

--- src/js/utils/generateListings.mjs
+++ src/js/utils/generateListings.mjs
@@ -1,7 +1,7 @@
 
 
 function generatePostHtml(post) {
-  const {id, title, body, media} = post;
+  const {id, title, body, description, media} = post;
 
   const listingCard = document.createElement("div");
   listingCard.className = "card card-width";
@@ -13,6 +13,27 @@ function generatePostHtml(post) {
     listingCard.appendChild(image);
   }
 
+  const cardBody = document.createElement("div");
+  cardBody.className = "card-body";
+
+  const cardTitle = document.createElement("h5");
+  cardTitle.className = "card-title";
+  cardTitle.textContent = title;
+
+  const cardDesc = document.createElement("p");
+  cardDesc.className = "card-text";
+  cardDesc.textContent = description;
+
+
+  const cardButton = document.createElement("a");
+  cardButton.className = "btn btn-primary"
+  cardButton.textContent = "View";
+
+  cardBody.appendChild(cardTitle);
+  cardBody.appendChild(cardDesc);
+  cardBody.appendChild(cardButton);
+
+  listingCard.appendChild(cardBody);
 
   return listingCard;
 };
